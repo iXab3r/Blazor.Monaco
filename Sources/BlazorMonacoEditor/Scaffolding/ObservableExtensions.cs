5f0f4752d6297499b0759a0b46e0a6ade83d74eb
Xab3r <mail.xaber@gmail.com>
2024-08-02 13:13:31 +0200

    FromAsync now uses ignoreExceptionsAfterUnsubscribe = true

--- Sources/BlazorMonacoEditor/Scaffolding/ObservableExtensions.cs
+++ Sources/BlazorMonacoEditor/Scaffolding/ObservableExtensions.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Reactive.Concurrency;
 using System.Reactive.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -9,17 +10,17 @@ internal static class ObservableExtensions
 {
     public static IObservable<T1> SelectAsync<T, T1>(this IObservable<T> observable, Func<T, CancellationToken, Task<T1>> supplier)
     {
-        return observable.Select(x => Observable.FromAsync((token) => supplier(x, token)).Take(1)).Concat();
+        return observable.Select(x => Observable.FromAsync((token) => supplier(x, token), new TaskObservationOptions(scheduler: null, ignoreExceptionsAfterUnsubscribe: true)).Take(1)).Concat();
     }
     
     public static IDisposable SubscribeAsync<T>(this IObservable<T> observable, Func<T, CancellationToken, Task> supplier, Action<Exception> onError)
     {
-        return observable.Select(x => Observable.FromAsync(token => supplier(x, token)).Take(1)).Concat().Subscribe(_ => { }, onError);
+        return observable.Select(x => Observable.FromAsync(token => supplier(x, token), new TaskObservationOptions(scheduler: null, ignoreExceptionsAfterUnsubscribe: true)).Take(1)).Concat().Subscribe(_ => { }, onError);
     }
     
     public static IDisposable SubscribeAsync<T>(this IObservable<T> observable, Func<T, Task> supplier, Action<Exception> onError)
     {
-        return observable.Select(x => Observable.FromAsync(_ => supplier(x)).Take(1)).Concat().Subscribe(_ => { }, onError);
+        return observable.Select(x => Observable.FromAsync(_ => supplier(x), new TaskObservationOptions(scheduler: null, ignoreExceptionsAfterUnsubscribe: true)).Take(1)).Concat().Subscribe(_ => { }, onError);
     }
     
     public static IDisposable SubscribeAsync<T>(this IObservable<T> observable, Func<T, CancellationToken, Task> supplier)
